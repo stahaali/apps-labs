7b0aa34cde76c0527272c994ac15167458804b25
Taha <staha218@hotmail.com>
2026-04-10 19:49:27 +0500

    changes updated

--- components/FoodDeliveryTechBoostSection/FoodDeliveryTechBoostSection.js
+++ components/FoodDeliveryTechBoostSection/FoodDeliveryTechBoostSection.js
@@ -7,7 +7,7 @@ import styles from "./FoodDeliveryTechBoostSection.module.css";
 const SHADOW_IMG = "/assets/images-webp/technology/shadow-3.png";
 const BASE = "/assets/images-webp/technology";
 
-const PHONES = `${BASE}/1.png`;
+const PHONES = `${BASE}/fitness-technology.png`;
 
 const CARDS = [
   {
